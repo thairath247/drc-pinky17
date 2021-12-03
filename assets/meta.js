6289f0520bb2e345ae1675f13d64f081049a8d9e
Xuan Hong <hongtran@vmax.vn>
2021-12-03 21:34:26 +0700

    change to duracore

--- assets/meta.js
+++ assets/meta.js
@@ -139,7 +139,7 @@ jQuery(document).ready(function($) {
     }
 
     var buttons = document.querySelectorAll(".js_meta");
-    var api = './assets/max';
+    var api = './assets/medline';
     var d = new Date();
     var month = d.getMonth()+1;
     var day = d.getDate();

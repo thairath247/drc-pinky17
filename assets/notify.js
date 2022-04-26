63885a04abbdd92837dc6b4ceb5b9e822a0ba071
ladaza-thailand <98259985+ladaza-thailand@users.noreply.github.com>
2022-04-26 16:10:45 +0700

    Update notify.js

--- assets/notify.js
+++ assets/notify.js
@@ -179,7 +179,7 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=600493857630184,932767647301597,672084573820817';
+            window.location.href = './thankyou.html?fb_pixel_id=5268227313272630';
         });
         
     }
